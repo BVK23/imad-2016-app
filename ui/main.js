c7a7f77b54712177ae899adce24fb5d31daf01ad
BVK23 <varunkrishna97@gmail.com>
2016-11-07 04:10:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,13 +5,17 @@ element.innerHTML = 'Almost constructed';
 //move img
 var img = document.getElementById('madi');
 var marginLeft = 0;
+var cnt = 1;
     function moveRight(){
         marginLeft = marginLeft + 1;
         img.style.marginLeft = marginLeft +'px';
     }
 img.onclick = function(){
-
-    
-    var interval = setInterval(moveRight, 50);
+    if(cnt == 1)
+    { var interval = setInterval(moveRight, 50);
+     cnt=2; 
+    }
+    else
+     {text-align: center;}
  
 };
