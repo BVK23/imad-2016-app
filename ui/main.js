92f2308e6ce4cf0c0e423ee21e08f171fd64d506
BVK23 <varunkrishna97@gmail.com>
2016-11-07 03:51:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element= document.getElementById('M-text');
 element.innerHTML = 'Almost constructed';
+
+//move img
+var img = document.getElementById('img');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
